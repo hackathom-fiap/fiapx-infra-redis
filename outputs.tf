46be703cee3d00c39c2e7a248e84a7fea7412726
hmouraf <humbertomoura@gmail.com>
2026-02-01 18:51:16 -0300

    fix redis

--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,24 @@
 output "redis_endpoint" {
-  description = "Endpoint primário do cluster Redis"
-  value       = aws_elasticache_cluster.redis.cache_nodes[0].address
+  description = "Endpoint primário do grupo de replicação Redis"
+  value       = aws_elasticache_replication_group.redis.primary_endpoint_address
 }
 
 output "redis_port" {
-  description = "Porta do cluster Redis"
-  value       = aws_elasticache_cluster.redis.port
+  description = "Porta do grupo de replicação Redis"
+  value       = aws_elasticache_replication_group.redis.port
 }
 
-output "redis_auth_token_secret_arn" {
-  description = "ARN do segredo no AWS Secrets Manager que contém a senha do Redis"
-  value       = aws_secretsmanager_secret.redis_auth_token.arn
+output "elasticache_user_group_id" {
+  description = "O ID do grupo de usuários do ElastiCache."
+  value       = aws_elasticache_user_group.default.id
+}
+
+output "elasticache_user_name" {
+  description = "O nome do usuário padrão do ElastiCache."
+  value       = aws_elasticache_user.default.user_name
+}
+
+output "elasticache_user_password_secret_arn" {
+  description = "ARN do segredo no AWS Secrets Manager que contém a senha do usuário."
+  value       = aws_secretsmanager_secret.user_password.arn
 }
--- variables.tf
+++ variables.tf
@@ -1,29 +1,24 @@
 variable "aws_region" {
   description = "Região da AWS para criar os recursos"
   type        = string
-  default     = "us-east-1"
 }
 
 variable "project_name" {
   description = "Nome do projeto para usar em tags e nomes de recursos"
   type        = string
-  default     = "fiapx-redis"
 }
 
-variable "vpc_cidr_block" {
-  description = "Bloco CIDR para a VPC"
+variable "vpc_id" {
+  description = "O ID da VPC existente onde os recursos serão criados."
   type        = string
-  default     = "10.0.0.0/16"
 }
 
-variable "subnet_cidr_block" {
-  description = "Bloco CIDR para a subnet"
-  type        = string
-  default     = "10.0.1.0/24"
+variable "subnet_ids" {
+  description = "A lista de IDs de subnets existentes para o cluster ElastiCache."
+  type        = list(string)
 }
 
 variable "redis_node_type" {
   description = "Tipo de nó para o cluster Redis ElastiCache"
   type        = string
-  default     = "cache.t2.micro"
 }
